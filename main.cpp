af884562f8644dc981562d91a678f0a40a7067d1
Quentin123 <quentin.larouziere@student.junia.com>
2024-01-07 17:41:10 +0100

    r

--- main.cpp
+++ main.cpp
@@ -51,6 +51,33 @@ int main()
     thread thread1(move_rame, ref(Rame1), 16, 1.3, 8, L1);
     thread thread2(move_rame, ref(Rame2), 16, 1.3, 9, L1);
 
+    //Station 
+    sf::Texture stationTexture;
+    if (!stationTexture.loadFromFile("C:/station.png")) {
+        return -1; // Erreur de chargement de l'image de la station
+    }
+
+    // background
+    sf::Texture backgroundTexture;
+
+    if (!backgroundTexture.loadFromFile("C:/image.jpg")) {
+        
+    }
+
+    // Charger une texture pour l'objet
+    sf::Texture objectTexture;
+    if (!objectTexture.loadFromFile("C:/metro.png")) {
+        return -2; // Erreur de chargement de l'image de l'objet
+    }
+
+    // Créer un sprite pour l'arrière-plan
+    sf::Sprite background;
+    background.setTexture(backgroundTexture);
+
+    sf::Sprite metro1(objectTexture);
+
+
+
     while (window.isOpen())
     {
         // Gére les événements
@@ -62,26 +89,26 @@ int main()
         }
 
         window.clear();
+        window.draw(background);
 
         for(auto start = L1.begin(); start != L1.end(); start++){
-            sf::CircleShape station;
-            station.setRadius(20);
-            station.setFillColor(sf::Color::Green);
-            station.setPosition(start->get_x()-20, start->get_y()-20);
+            sf::Sprite station(stationTexture);
+            station.setPosition(start->get_x() - 20, start->get_y() - 20);
             window.draw(station);
         }
 
-        sf::RectangleShape r1(sf::Vector2f(30.0f, 10.0f));
-        r1.setFillColor(sf::Color::Red);
-        r1.setRotation(Rame1.get_arg(L1));
-        r1.setPosition(Rame1.get_x()+15, Rame1.get_y()+5);
+
+       
+        metro1.setRotation(Rame1.get_arg(L1));
+        metro1.setPosition(Rame1.get_x()+15, Rame1.get_y()+5);
 
         sf::RectangleShape r2(sf::Vector2f(30.0f, 10.0f));
         r2.setFillColor(sf::Color::Red);
         r2.setRotation(Rame2.get_arg(L1));
-        r2.setPosition(Rame2.get_x()+15, Rame2.get_y()+5);
+        r2.setPosition(Rame2.get_x()*3+15, Rame2.get_y()*3+5);
 
-        window.draw(r1);
+        
+        window.draw(metro1);
         window.draw(r2);
 
         window.display();
